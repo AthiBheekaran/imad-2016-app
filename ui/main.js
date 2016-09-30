d49e5a408554fbbb14e6b1ff876460c46131e82f
Gautham V P <vpgautham@gmail.com>
2016-09-30 20:03:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,7 +6,7 @@ var element = document.getElementById('main-text');
 element.innerHTML = 'new value';
 
 //Move Image
-var img = document.getElementById('img');
+var img = document.getElementById('madi');
 img.onclick = function () {
     img.style.marginleft = '150px';
 }

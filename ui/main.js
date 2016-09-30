b300e467511a5170d85e48f64a54a66a2f601ba6
Gautham V P <vpgautham@gmail.com>
2016-09-30 19:58:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,12 @@
 console.log('Loaded!');
+
+//Change tet of main div 
+var element = document.getElementById('main-text');
+
+element.innerHTML = 'new value';
+
+//Move Image
+var img = document.getElementById('img');
+img.onclick = function () {
+    img.style.marginleft = '100px';
+}
